207a4ba78fb51005fd30cf2996aaefa221b1c6dd
kevinfigueredo <figueredo.kevin2000@gmail.com>
2022-04-28 20:01:16 -0300

    NavBar

--- src/NavBar/NavBar.jsx
+++ src/NavBar/NavBar.jsx
@@ -1,41 +1,56 @@
-import { Container, Row } from "react-bootstrap"
+import { Container, Navbar, Row } from "react-bootstrap"
 import "./NavBar.css";
 
-export const NavBar=()=>{
+export const NavBar = () =>{
+    let ubicacionPrincipal = window.pageYOffset;
+    window.onscroll = function(){
+        let Desplazamiento_Actual = window.pageYOffset;
+        if(ubicacionPrincipal >= Desplazamiento_Actual){
+            document.getElementById('nav').classList = 'py-3 myStyle';
+            document.getElementById('nav').style.top = '0';
+        }else{
+            document.getElementById('nav').classList = 'py-3 myStyle';
+            document.getElementById('nav').style.top = '-100px';
+        }
+        ubicacionPrincipal = Desplazamiento_Actual;
+    }
+    function clickearCerrar(){
+        document.getElementById("nav-ul").classList.toggle("show")
+        document.getElementById("nav-ul").classList.remove("d-none");
+    }
     return(
-        <header className="navb">
+        <div className="bg-black py-3" id="nav">
             <Container>
-                <Row>
-                    <nav className="navbar-expand-md navbar">
-                        <div class="col-sm-2">
-                            <a href="#"><img src="imgs/LOGO_DE_LA_CRUZ_BASE_NEGRO.jpg" alt="" className="img-fluid"/></a>
-                        </div>
-                        <div className="col-sm-8">
-                            <ul className="nav navbar text-right">
-                                <li className="nav-item mx-3">
-                                    <a href="">Inicio</a>
-                                </li>
-                                <li className="nav-item mx-3">
-                                    <a href="">Sobre nosotros</a>
-                                </li>
-                                <li className="nav-item mx-3">
-                                    <a href="">Servicios</a>
-                                </li>
-                                <li className="nav-item mx-3">
-                                    <a href="">Tienda</a>
-                                </li>
-                                <li className="nav-item mx-3">
-                                    <a href="#">Turnos</a>
-                                </li>
-                                <li className="nav-item mx-3">
-                                    <a href="">Contacto</a>
-                                </li>
-                            </ul>
-                            
-                        </div>
-                    </nav>
-                </Row>
+                    <Navbar>
+                        <button className="hamburger" id="hamburger" onClick={()=>{
+                                document.getElementById("nav-ul").classList.toggle("show")
+                                document.getElementById("nav-ul").classList.remove("d-none");
+                            }}>
+                            <FontAwesomeIcon className="fas fa-bars" icon={faBars}/>
+                        </button>
+                        <h1 className="nombreH1">Ruca</h1>
+                        <ul className="nav nav-ul" id="nav-ul">
+                            <li className="nav-item ">
+                                <Link to={} className="nav-link item" onClick={clickearCerrar}>Inicio</Link>
+                            </li>
+                            <li className="nav-item ">
+                                <Link to={} className="nav-link item" onClick={clickearCerrar}>Sobre Nosotros</Link>
+                            </li>
+                            <li className="nav-item ">
+                                <Link to={} className="nav-link item" onClick={clickearCerrar}>Servicios</Link>
+                            </li>
+                            <li className="nav-item ">
+                                <Link to={} className="nav-link item" onClick={clickearCerrar}>Tienda</Link>
+                            </li>
+                            <li className="nav-item ">
+                                <Link to={} className="nav-link item" onClick={clickearCerrar}>Turnos</Link>
+                            </li>
+                            <li className="nav-item ">
+                                <Link to={} className="nav-link item" onClick={clickearCerrar}>Contacto</Link>
+                            </li>
+                        </ul>
+                    </Navbar>
             </Container>
-        </header>
+        </div>
     )
 }
